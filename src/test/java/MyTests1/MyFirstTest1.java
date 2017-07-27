08b09c6dea5b300b4d877a8c30dfb86c6e0ccfcf
VinodRajapantula <rvinodqa2009@gmail.com>
2017-07-27 09:38:48 +0530

    maven project

---
+++ src/test/java/MyTests1/MyFirstTest1.java
@@ -0,0 +1,35 @@
+package MyTests1;
+
+
+
+public class MyFirstTest1 {
+	
+	/*@BeforeTest
+	public void BeforeTest1(){
+		System.out.println("BeforeTest");	
+	}
+	
+	@BeforeMethod
+	public void BeforeMethod1(){
+		System.out.println("BeforeMethod");	
+	}
+	
+	
+	@Test
+	public void test1(){
+		System.out.println("test1");
+		
+	}
+	
+	@Test
+	public void test2(){
+		System.out.println("test2");
+		
+	}*/
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello world");
+	}
+
+}
